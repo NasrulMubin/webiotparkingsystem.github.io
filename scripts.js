b0c2bb488f3dda92835d6ee487caa6e07c51fe87
NasrulMubin <86328915+NasrulMubin@users.noreply.github.com>
2021-06-23 06:12:48 +0700

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,15 @@
+/*!
+* Start Bootstrap - Scrolling Nav v5.0.1 (https://startbootstrap.com/template/scrolling-nav)
+* Copyright 2013-2021 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-scrolling-nav/blob/master/LICENSE)
+*/
+//
+// Scripts
+// 
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Activate Bootstrap scrollspy on the main nav element
+    var d = new Date();
+    document.getElementById("datetime").innerHTML = d.toLocaleString();
+});
